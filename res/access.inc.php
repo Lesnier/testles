b59fead971037fe45fc52b957639ae9984c4f352
Lesnier <lesniergonzalez@gmail.com>
2016-05-24 15:21:43 -0500

    Add files via upload

---
+++ res/access.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+// Users data
+$imSettings['access']['webregistrations_gid'] = '9rl8r237';
+$imSettings['access']['users'] = array(
+	'admin' => array(
+		'groups' => array('v0j32023'),
+		'id' => 'v0j32023',
+		'name' => 'Admin',
+		'password' => '99iy9c1x',
+		'email' => '',
+		'page' => 'index.html'
+	),
+	'nuevousuario' => array(
+		'groups' => array('54jj155j'),
+		'id' => '54jj155j',
+		'name' => 'Nuevousuario',
+		'password' => 'xe25faad',
+		'email' => '',
+		'page' => 'index.html'
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('v0j32023');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
---
+++ res/x5settings.js
@@ -0,0 +1,114 @@
+(function ( $, x5engine ) {
+	var x = x5engine,
+		s = x.settings,
+		p = s.currentPath,
+		b = x.boot;
+
+	s.siteId = '11978937FE83DE14D0A6E35DF10CF60F';
+	b.push(function () {
+		x.setupDateTime();
+		x.imAccess.showLogout();
+		x.utils.autoHeight();
+		x.cart.ui.updateWidget();
+		x.imGrid.init();
+	});
+	s.menu = {
+		verticalScroll: false,
+		orientation: 'vertical'
+	};
+	b.push(function () {
+		x.menu({
+			target: '#imMnMn',
+			showCurrent: false,
+			desktopVerticalScroll: false,
+			mobileVerticalScroll: false,
+			showLogoOnScroll: false,
+			orientation: 'vertical',
+			menuHeight: 37,
+			menuWidth: 130,
+			submenuHeight: 22,
+			submenuWidth: 160,
+			opacity: 0.8,
+			type: 'singleColumn',
+			alignment: 'right',
+			effect: 'fade'
+		});
+	});
+	b.push(function () { x.utils.imPreloadImages([p + 'menu/hor_main.png',p + 'menu/hor_main_h.png',p + 'menu/hor_main_c.png',p + 'menu/ver_main.png',p + 'menu/ver_main_h.png',p + 'menu/ver_main_c.png', p + 'res/imLoad.gif', p + 'res/imClose.png']); });
+
+	// ShowBox
+	$.extend(s.imShowBox, {
+		'effect' : 'fade',
+		'shadow' : '',
+		'background' : '#000000',
+		'borderWidth' : {
+			'top': 1,
+			'right': 1,
+			'bottom': 1,
+			'left': 1
+		},
+		buttonRight: {
+			url: p + 'res/b01_r.png',
+			position: {
+				x: -12,
+				y: 0
+			}
+		},
+		buttonLeft: {
+			url: p + 'res/b01_l.png',
+			position: {
+				x: -12,
+				y: 0
+			}
+		},
+		'borderRadius' : '5px 5px 5px 5px',
+		'borderColor' : '#000000 #000000 #000000 #000000',
+		'textColor' : '#000000',
+		'fontFamily' : 'Tahoma',
+		'fontStyle' : 'normal',
+		'fontWeight' : 'normal',
+		'fontSize' : '9pt',
+		'textAlignment' : 'left',
+		'boxColor' : '#FFFFFF',
+		'opacity' : 0.7,
+		'radialBg' : true // Works only in Mozilla Firefox and Google Chrome
+	});
+
+	// PopUp
+	$.extend(s.imPopUp, {
+		'effect' : 'fade',
+		'width' : 500,
+		'shadow' : '',
+		'background' : '#000000',
+		'borderRadius' : 10,
+		'textColor' : '#000000',
+		'boxColor' : '#FFFFFF',
+		'opacity' : 0.7
+	});
+
+	// Tip
+	$.extend(s.imTip, {
+		'borderRadius' : 1,
+		'arrow' : true,
+		'position' : 'bottom',
+		'effect' : 'none',
+		'showTail' : true
+	});
+
+	// Captcha
+	b.push(function () {
+		x5engine.captcha.instance = new x5engine.captcha.x5captcha({
+			"phpfile": "captcha/x5captcha.php",
+		});
+	}, false, 'first');
+
+	// BreakPoints
+	s.breakPoints.push({ "hash": "02be155abd7db86cd86cc0c959970f09", "name": "Desktop", "start": "max", "end": 1190, "fluid": false});
+	s.breakPoints.push({ "hash": "eee935b95a745d19534ef1597767c120", "name": "Breakpoint 1", "start": 1189, "end": 840, "fluid": false});
+	s.breakPoints.push({ "hash": "b5a6e70dcd45b271d4dbff444ddeed77", "name": "Mobile", "start": 839, "end": 480, "fluid": false});
+	s.breakPoints.push({ "hash": "e81e7c6a4cc72fa678403ac3f73710af", "name": "Mobile Fluid", "start": 479, "end": 0, "fluid": true});
+
+	b.push(function () { x.cookielaw.activateScripts() });
+
+	s.loaded = true;
+})( _jq, x5engine );
